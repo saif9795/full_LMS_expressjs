e08cd9e99cb4e6f2d58eb5523aabc848071b0cd4
saif9795 <adnan.saif9795@gmail.com>
2025-12-19 23:27:10 +0600

    utils folder has been created : apiError, apiResponse, asyncHandler

--- app.js
+++ app.js
@@ -6,10 +6,14 @@ import cookieParser from 'cookie-parser';
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+    origin : process.env.CORS_ORIGIN
+}
+));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
+app.use(express.static("public"));
 
 export {app}
 
