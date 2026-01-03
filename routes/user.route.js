e34b8c0f65c336931de2750a302e49412070b9bc
saif9795 <adnan.saif9795@gmail.com>
2026-01-03 22:37:41 +0600

    logoutUser controller developed

--- routes/user.route.js
+++ routes/user.route.js
@@ -1,6 +1,7 @@
 import { Router } from "express";
-import { loginUser, registerUser } from "../controllers/user.controller.js";
+import { loginUser, logoutUser, registerUser } from "../controllers/user.controller.js";
 import { upload } from './../middlewares/multer.middleware.js';
+import { verifyJWT } from "../middlewares/auth.middleware.js";
 
 const router = Router();
 
@@ -8,5 +9,7 @@ router.route("/register").post(
       upload.single("avatar"), registerUser);
 
 router.route("/login").post(loginUser);
+
+router.route("/logout").post(verifyJWT, logoutUser);
 
 export default router;
