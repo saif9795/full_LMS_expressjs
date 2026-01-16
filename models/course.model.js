8bc39b0726272ba1ba8f5679c09469ad04e3cf38
saif9795 <adnan.saif9795@gmail.com>
2026-01-17 02:26:55 +0600

    course model has been updated

--- models/course.model.js
+++ models/course.model.js
@@ -1,7 +1,34 @@
 import mongoose from "mongoose";
 
 const courseSchema = new mongoose.Schema({
-    
+    courseTitle:{
+        type: String,
+        required: true,
+    },
+    description:{
+        type: String,
+        required: true,
+    },
+    courseImageUrl:{
+        type: String,
+    },
+    price:{
+        type: Number,
+        required: true,
+    },
+    instructor:{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+        required: true,
+    },
+    lessons: [
+        {
+            type: mongoose.Schema.Types.ObjectId,   
+            ref: "Lessons",
+        }
+    ],
+},{
+    timestamps: true,   
 });
 
 const Course = mongoose.model("Course", courseSchema);
