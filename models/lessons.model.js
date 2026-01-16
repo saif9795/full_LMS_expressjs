8f8cab5379f7fe4fa644e2742df48bbca00c73c5
saif9795 <adnan.saif9795@gmail.com>
2026-01-17 02:21:51 +0600

    lessons model updated

--- models/lessons.model.js
+++ models/lessons.model.js
@@ -1,7 +1,19 @@
 import mongoose from "mongoose";
 
 const lessonsSchema = new mongoose.Schema({
-    
+    title:{
+        type: String,
+        required: true,
+    },
+    contentType:{
+        type: String,
+        enum: ['video', 'article', "audio", 'quiz'],
+    },
+    contentUrl:{
+        type: String,
+    },
+},{
+    timestamps: true,
 });
 
 const Lessons = mongoose.model("Lessons", lessonsSchema);
